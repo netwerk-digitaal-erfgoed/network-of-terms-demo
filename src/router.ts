b5673fb40a8a3370aeda2e4f09b48579e2b3d662
David de Boer <david@ddeboer.nl>
2020-10-03 09:34:52 +0200

    Link beta button and scroll to prototype section (#14)

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Faq from './components/Faq.vue'
 import Home from './components/Home.vue'
+import Faq from './components/Faq.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
@@ -10,12 +10,19 @@ export const router = createRouter({
       path: '/',
       name: 'home',
       component: Home,
-      props: true
     },
     {
       path: '/faq',
       name: 'faq',
-      components: { default: Faq }
+      component: Faq,
     }
-  ]
+  ],
+  scrollBehavior (to) {
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: 'smooth',
+      }
+    }
+  },
 })
