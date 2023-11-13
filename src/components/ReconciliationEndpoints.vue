3d718f25625df866788ecad64d2c4f4cdd270e60
David de Boer <david@ddeboer.nl>
2023-11-13 10:46:00 +0100

    feat: Link to source
    Fix #927

--- src/components/ReconciliationEndpoints.vue
+++ src/components/ReconciliationEndpoints.vue
@@ -10,11 +10,20 @@
         <h5 class="mb-1">{{ source.name }}
           <small class="text-muted">{{ source.creators[0].alternateName }}</small>
         </h5>
-        <CopyButton
-          :text="source.features[0].url"
-          class="btn btn-sm btn-primary"
-          label="faq.copyUrl"
-        />
+        <div>
+          <CopyButton
+            :text="source.features[0].url"
+            class="btn btn-sm btn-primary"
+            label="faq.copyUrl"
+          />
+          <a
+            :href="source.mainEntityOfPage"
+            class="btn btn-sm btn-primary ml-2"
+          >
+            {{ t('search.viewAtSource') }}
+            <ExternalLinkIcon class="icon" />
+          </a>
+        </div>
       </div>
       <p>{{ source.description }}</p>
       <pre class="small mb-1 text-muted pre-scrollable">{{ source.features[0].url }}</pre>
@@ -28,16 +37,17 @@ import {defineComponent, ref} from 'vue';
 import {Feature, Source} from '../query';
 import {useI18n} from 'vue-i18n';
 import CopyButton from './CopyButton.vue';
+import {ExternalLinkIcon} from '@heroicons/vue/outline';
 
 export default defineComponent({
   name: "ReconciliationEndpoints",
-  components: {CopyButton},
+  components: {ExternalLinkIcon, CopyButton},
   async setup() {
     const {t} = useI18n();
     useClient({
       url: import.meta.env.VITE_GRAPHQL_URL,
     });
-    const result = await useQuery({query: 'query Sources { sources { name alternateName description uri creators { uri alternateName } features { type url } } }'});
+    const result = await useQuery({query: 'query Sources { sources { name alternateName mainEntityOfPage description uri creators { uri alternateName } features { type url } } }'});
     const reconciliationSources = result.data.value.sources.filter((source: Source) => source.features.some((feature: Feature) => feature.type === 'RECONCILIATION'));
 
     return {t, reconciliationSources, copied: ref(false)};
@@ -49,5 +59,8 @@ export default defineComponent({
 svg {
   width: 1em;
   height: 1em;
+}
+a.btn {
+  text-decoration: none;
 }
 </style>
